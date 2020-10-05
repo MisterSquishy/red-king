6200cdd10daee4f949b14487f51275b2941e9502
Pete Davids <pdavids@drift.com>
2020-10-04 20:35:12 -0400

    better react configs

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,3 +1,9 @@
-export default {
+const prod = {
   ENDPOINT: "http://red-king.herokuapp.com"
 }
+
+const dev = {
+  ENDPOINT: "http://localhost:3000"
+};
+
+export default process.env.NODE_ENV === "development" ? dev : prod;
