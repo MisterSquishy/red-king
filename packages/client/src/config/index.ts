b051a8bc7d3bd11105c88ef070ff644e16684527
Raymond Cox <raymond@raymondjcox.com>
2021-02-24 12:07:39 -0500

    cra

--- packages/client/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-const prod = {
-  ENDPOINT: "http://red-king.herokuapp.com",
-};
-
-const dev = {
-  ENDPOINT: "http://localhost:3000",
-};
-
-export default process.env.NODE_ENV === "development" ? dev : prod;
